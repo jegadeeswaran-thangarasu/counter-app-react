8a2bcdf734180dc79f031c462cbbaf7a9952978c
jegadeeswaran-ta <jegadeeswaran.ta@coda.global>
2022-01-16 13:55:12 +0530

    nav bar component added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
-import './App.css';
 import { Route, Redirect, Switch } from 'react-router-dom';
 // import Counter from './components/counter';
 import Movies from './components/movies';
 import Rentals from './components/rentals';
 import Customers from './components/customers';
 import NotFound from './components/not-found';
+import './App.css';
+import NavBar from './components/nav-bar';
+import React from 'react';
 
 function App() {
   return (
+    <React.Fragment>
+      <NavBar />
       <main className='container'>
         <Switch>
           <Route path="/movies" component={Movies}></Route>
@@ -18,6 +22,7 @@ function App() {
           <Redirect to="/not-found" />
         </Switch>
       </main>
+    </React.Fragment>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -22,3 +22,23 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+{/* <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <Link className="navbar-brand" to="/">Vidly</Link>
+            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+                <div className="navbar-nav">
+                    <NavLink className="nav-item nav-link" to="/movies">
+                        Movies
+                    </NavLink>
+                    <NavLink className="nav-item nav-link text-danger" to="/customers">
+                        Customers
+                    </NavLink>
+                    <NavLink className="nav-item nav-link" to="/rentals">
+                        Rentals
+                    </NavLink>
+                </div>
+            </div>
+        </nav> */}
